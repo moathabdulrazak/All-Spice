19662c03d795c73103e710cb619c2c49bb49f18b
moathabdulrazak <moathabdulrazak12@gmail.com>
2023-01-25 20:12:03 -0700

    main functionalioty all done. few strech goals

--- allSpicee.client/src/components/IngredientCard.vue
+++ allSpicee.client/src/components/IngredientCard.vue
@@ -1,10 +1,11 @@
 <template>
 
-<div class="col-6 ">
-<h6> name: {{ ingredient.name }} quantity: {{ ingredient.quantity }}</h6>
-<button title="delete event" class=" btn btn-outline btn-danger mdi mdi-delete"
-         @click="deleteIngredient"></button>
-</div>
+  <div class="col-6 ">
+    <h6> name: {{ ingredient.name }} quantity: {{ ingredient.quantity }}</h6>
+    <button v-if="ingredient.accountId == account.id" title="delete event"
+      class=" btn btn-outline btn-danger mdi mdi-delete" @click="deleteIngredient"></button>
+
+  </div>
 
 </template>
 
@@ -16,13 +17,17 @@ import { ingrediantService } from "../services/IngrediantService.js";
 export default {
   props: {
     ingredient: { type: Object, required: true }
+
   },
-  setup(props){
-  return { 
-    async deleteIngredient(){
-      await ingrediantService.removeIngredient(props.ingredient.id)
+  setup(props) {
+    return {
+      ingredients: computed(() => AppState.ingredients),
+      account: computed(() => AppState.account),
+      async deleteIngredient() {
+        await ingrediantService.removeIngredient(props.ingredient.id)
+      }
+
     }
-   }
   }
 };
 </script>
